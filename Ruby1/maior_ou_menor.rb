676ed6d5c15bb801bd390401e5cbd3c24ad1260d
phill9242 <philcarvalho@live.com>
2023-05-08 15:52:41 -0300

    feature: tentativa
    adicionado o número total de tentativas

--- Ruby1/maior_ou_menor.rb
+++ Ruby1/maior_ou_menor.rb
@@ -1,3 +1,6 @@
+# função integer para levantar uma exception
+# caso o input do usuário não seja um número, uma exceção é levantada com uma mensagem
+# caso seja um inteiro, faz uma verificação se o número está dentro do range
 def numero_invalido(numero)
     begin
       numero_int = Integer(numero)
@@ -12,17 +15,17 @@ def numero_invalido(numero)
   end
 
 # puts coloca automaticamente uma quebra de linha ao final
-puts    "Qual seu nome ?\n"
+puts    "Qual seu nome ?"
 
 #o método chomp remove a quebra de linha final de uma string
 nome = gets.chomp
 
-puts    "Bem-vindo, #{nome}. \n\nVamos jogar um jogo !"
-puts    "Irei escolher um número entre 0 e 200"
+puts    "\nBem-vindo, #{nome}. \nVamos jogar um jogo !"
+puts    "\nIrei escolher um número entre 0 e 200"
 
 #a função rand escolhe um número aleatório. 
 # Caso apenas um parâmetro seja passado, o range inicial é 0, sendo seguido de números inteiros conseguintes até o limite passado
-numero_aleatorio = rand(201)
+numero_secreto = rand(201)
 
 #um loop para emular a "escolha" do computador
 #a melhor saída nesse caso é utilizar print, pois não adiciona a quebra de linha ao final entre cada ponto
@@ -31,20 +34,30 @@ for i in 0 .. 2
     sleep (1)
 end
 
-puts "\nPronto.\nEscolha seu número e vamos começar:"
+puts "\nPronto! Escolha seu número e vamos começar:\n\n"
 
-while (1)
+# Laço que fica em loop até que o número escolhido pelo usuário esteja correto
+# devolve mensagens personalizadas dependendo do palpite do usuário ser correto ou não
+acerto = false
+limite_tentativa = 5
+for tentativa in 1..limite_tentativa
+    puts "Tentativa #{tentativa} de #{limite_tentativa} "
     numero_usuario = gets.chomp
     if numero_invalido (numero_usuario)
-        puts "Por favor, escolha um número entre 0 e 200 !"
-    elsif numero_usuario.to_i < numero_aleatorio
-        puts "Seu número está abaixo !\n"
-    elsif numero_usuario.to_i > numero_aleatorio
-        puts "Seu número está acima !\n"   
+        puts "Por favor, escolha um número entre 0 e 200 !\n"
+    elsif numero_usuario.to_i < numero_secreto
+        puts "\nSeu número está abaixo !"
+    elsif numero_usuario.to_i > numero_secreto  
+        puts "\nSeu número está acima !"   
     else
-        puts "Parabéns, o número que eu escolhi era #{numero_aleatorio}! \n"   
+        puts "Parabéns, o número que eu escolhi era #{numero_secreto}! \n" 
+        acerto = true  
         break
     end
-end
     
+end
+
+if acerto == false
+    puts "\nVocê perdeu :(\nO Número escolhido era #{numero_secreto} "
+end
 
