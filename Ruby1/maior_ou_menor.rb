0f011e20dc58bd465ac3f62b7ecaddd2cfb2a2c2
phill9242 <philcarvalho@live.com>
2023-05-08 16:50:41 -0300

    refatoração

--- Ruby1/maior_ou_menor.rb
+++ Ruby1/maior_ou_menor.rb
@@ -1,63 +1,65 @@
-# função integer para levantar uma exception
-# caso o input do usuário não seja um número, uma exceção é levantada com uma mensagem
-# caso seja um inteiro, faz uma verificação se o número está dentro do range
-def numero_invalido(numero)
+def dar_boas_vindas()
+    puts    "Qual seu nome ?"
+    nome = gets.chomp
+    puts    "\nBem-vindo, #{nome}. \nVamos jogar um jogo !"
+    puts    "\nIrei escolher um número entre 0 e 200"
+end
+
+def sortear_numero_secreto()
+    for i in 0 .. 2
+        print "."
+        sleep (1)
+    end
+    numero_sorteado = rand(201)
+    puts "\nPronto! Escolha seu número e vamos começar:\n\n"
+    return (numero_sorteado)
+end
+
+def checar_numero(numero)
     begin
       numero_int = Integer(numero)
       if numero_int < 0 || numero_int > 200
-        return true
+        puts "Escolha um número entre 0 e 200\n\n"
+        return false
       end
     rescue ArgumentError
-      puts "Apenas números são aceitos!"
-      return true
+      puts "Apenas números são aceitos!\n\n"
+      return false
     end
-    return false
+    return true
+end
+
+def pegar_numero_usuario
+    begin
+      numero_usuario = gets.chomp
+    end while checar_numero(numero_usuario) == false
+    numero_usuario
   end
-
-# puts coloca automaticamente uma quebra de linha ao final
-puts    "Qual seu nome ?"
-
-#o método chomp remove a quebra de linha final de uma string
-nome = gets.chomp
-
-puts    "\nBem-vindo, #{nome}. \nVamos jogar um jogo !"
-puts    "\nIrei escolher um número entre 0 e 200"
-
-#a função rand escolhe um número aleatório. 
-# Caso apenas um parâmetro seja passado, o range inicial é 0, sendo seguido de números inteiros conseguintes até o limite passado
-numero_secreto = rand(201)
-
-#um loop para emular a "escolha" do computador
-#a melhor saída nesse caso é utilizar print, pois não adiciona a quebra de linha ao final entre cada ponto
-for i in 0 .. 2
-    print "."
-    sleep (1)
-end
-
-puts "\nPronto! Escolha seu número e vamos começar:\n\n"
-
-# Laço que fica em loop até que o número escolhido pelo usuário esteja correto
-# devolve mensagens personalizadas dependendo do palpite do usuário ser correto ou não
-acerto = false
-limite_tentativa = 5
-for tentativa in 1..limite_tentativa
-    puts "Tentativa #{tentativa} de #{limite_tentativa} "
-    numero_usuario = gets.chomp
-    if numero_invalido (numero_usuario)
-        puts "Por favor, escolha um número entre 0 e 200 !\n"
-    elsif numero_usuario.to_i < numero_secreto
-        puts "\nSeu número está abaixo !"
-    elsif numero_usuario.to_i > numero_secreto  
-        puts "\nSeu número está acima !"   
-    else
-        puts "Parabéns, o número que eu escolhi era #{numero_secreto}! \n" 
-        acerto = true  
-        break
-    end
     
-end
-
-if acerto == false
+def adivinhar_numero (numero_secreto)
+    limite_tentativa = 5
+    tentativa = 1
+    while tentativa <= limite_tentativa
+        puts "Tentativa #{tentativa} de #{limite_tentativa} "
+        numero_usuario = pegar_numero_usuario()
+        if numero_usuario.to_i < numero_secreto
+            puts "\nSeu número está abaixo !"
+        elsif numero_usuario.to_i > numero_secreto  
+            puts "\nSeu número está acima !"   
+        else
+            puts "Parabéns, o número que eu escolhi era #{numero_secreto}! \n" 
+            return
+        end
+        tentativa += 1
+    end
     puts "\nVocê perdeu :(\nO Número escolhido era #{numero_secreto} "
 end
 
+def maior_ou_menor()
+    dar_boas_vindas()
+    numero_secreto = sortear_numero_secreto()
+    adivinhar_numero(numero_secreto)
+
+end
+
+maior_ou_menor()
