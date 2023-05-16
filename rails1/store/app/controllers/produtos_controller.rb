4d1edbe939cc8a9146e2c2bad1edff092501eea3
phill9242 <philcarvalho@live.com>
2023-05-16 12:08:44 -0300

    rails1: feature - alerts

--- rails1/store/app/controllers/produtos_controller.rb
+++ rails1/store/app/controllers/produtos_controller.rb
@@ -6,14 +6,25 @@ class ProdutosController < ApplicationController
     end
 
     def create
-        produto = params[:id]
-        Produto.create produto
-        redirect_to root_path
+        produto_params = params.require(:produto).permit(:nome, :descricao, :preco, :quantidade)
+        
+        @produto = Produto.new produto_params
+
+        if @produto.save
+            flash[:notice] = "Produto salvo"
+        end
+        render :new
     end
+      
 
     def destroy
         id = params.require(:id)
         Produto.destroy id
         redirect_to root_path
+    end
+
+    def busca
+        @nome = params[:nome]
+        @produtos = Produto.where "nome like ?", "%#{@nome}%"
     end
 end
--- rails1/store/config/routes.rb
+++ rails1/store/config/routes.rb
@@ -1,8 +1,7 @@
 
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
+    resources :produtos, only: [:new, :create, :destroy]
     root to: "produtos#index"
-    get "produtos/cadastro", to:"produtos#cadastro"
-    post "produtos", to:"produtos#create"
-    get "produtos/:id/remover", to:"produtos#destroy"
+    get "produtos/busca", to: "produtos#busca", as: :busca_produto
 end
