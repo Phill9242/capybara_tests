0d721a5371575de295a6d22454ed3c614694da07
phill9242 <philcarvalho@live.com>
2023-05-18 10:22:56 -0300

    rails1: test condiguration and class modifications in css

---
+++ rails1/storepostgree/test/support/capybara.rb
@@ -0,0 +1,5 @@
+require 'capybara/rails'
+
+Capybara.configure do |config|
+  config.default_max_wait_time = 5
+end
--- rails1/storepostgree/test/system/departamentos_test.rb
+++ rails1/storepostgree/test/system/departamentos_test.rb
@@ -3,6 +3,7 @@ require "application_system_test_case"
 class DepartamentosTest < ApplicationSystemTestCase
   setup do
     @departamento = departamentos(:one)
+    @departamento2 = departamentos(:two)
   end
 
   test "visitar página departamentos" do
@@ -10,26 +11,52 @@ class DepartamentosTest < ApplicationSystemTestCase
     assert_selector "h1", text: "Departamentos"
   end
 
-  test "creating a Departamento" do
+  test "Criar um Departamento" do
     visit departamentos_url
     click_on "Novo Departamento"
 
-    fill_in "Nome", with: @departamento.nome
+    fill_in "departamento_nome", with: "Eletrônicos"
     click_on "Criar Departamento"
 
-    assert_text "Departamento was successfully created"
+    assert_text "Departamento was successfully created."
     click_on "Voltar"
+
+    assert_selector ".nome_departamento", text: "Eletrônicos"
   end
 
-  test "updating a Departamento" do
-    visit departamentos_url
-    click_on "Editar", match: :first
+  test "Atualizar um Departamento" do
+    nome_atualizado = "Nome Unico de Registro teste"
 
-    fill_in "departamento_nome", with: @departamento.nome
+    visit departamentos_url
+    within('.departamento-mystring') do
+      click_on "Editar"
+    end
+
+    fill_in "departamento_nome", with: nome_atualizado
     click_on "Atualizar Departamento"
 
     assert_text "Departamento was successfully updated"
     click_on "Voltar"
+
+    assert_selector ".nome_departamento", exact_text: nome_atualizado
+  end
+
+  test "Atualizar o departamento mystring2" do
+    nome_atualizado = "Nome Unico de Registro teste"
+    
+    visit departamentos_url
+  
+    within('.departamento-mystring') do
+      click_on "Editar"
+    end
+  
+    fill_in "departamento_nome", with: nome_atualizado
+    click_on "Atualizar Departamento"
+  
+    assert_text "Departamento was successfully updated"
+    click_on "Voltar"
+  
+    assert_selector ".nome_departamento", exact_text: nome_atualizado
   end
 
   test "deletar um Departamento" do
@@ -40,4 +67,5 @@ class DepartamentosTest < ApplicationSystemTestCase
 
     assert_text "Departamento was successfully destroyed"
   end
+
 end
